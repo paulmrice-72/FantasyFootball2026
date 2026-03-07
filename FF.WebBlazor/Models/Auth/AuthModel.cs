c7953faa0e2b6ba4ba4034776fcefc13a8575760
Paul M. Rice <158781635+paulmrice-72@users.noreply.github.com>
2026-03-07 00:49:03 -0600

    login page!

---
+++ FF.WebBlazor/Models/Auth/AuthModel.cs
@@ -0,0 +1,19 @@
+﻿namespace FF.WebBlazor.Models.Auth;
+
+public record RegisterRequest(
+    string Email,
+    string Password,
+    string FirstName,
+    string LastName);
+
+public record LoginRequest(
+    string Email,
+    string Password);
+
+public record RefreshTokenRequest(
+    string RefreshToken);
+
+public record AuthResponse(
+    string AccessToken,
+    string RefreshToken,
+    DateTime AccessTokenExpiry);
--- FF.WebBlazor/Program.cs
+++ FF.WebBlazor/Program.cs
@@ -1,8 +1,27 @@
+using FF.WebBlazor;
+using FF.WebBlazor.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<FF.WebBlazor.App>("#app");
+builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// HTTP Client
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:64233/")
+});
+
+// Auth
+builder.Services.AddSingleton<TokenStore>();
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddAuthorizationCore();
+
+// MudBlazor
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
