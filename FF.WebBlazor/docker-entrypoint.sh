fb8e5cd844c6548279e300786b8f825956c310d2
Paul M. Rice <158781635+paulmrice-72@users.noreply.github.com>
2026-04-03 21:23:51 -0500

    Pmr deploy 004 (#459)
    * fix staging environment
    
    * added docker-encryption.sh
    
    * corrected name of file

---
+++ FF.WebBlazor/docker-entrypoint.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/sh
+if [ "$ASPNETCORE_ENVIRONMENT" = "Staging" ]; then
+  echo "Staging environment detected — swapping appsettings"
+  cp /usr/share/nginx/html/appsettings.Staging.json \
+     /usr/share/nginx/html/appsettings.json
+fi
+exec nginx -g "daemon off;"
