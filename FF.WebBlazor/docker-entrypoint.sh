8ab7a9256043d2a34c5619276c75e2db1e217d10
Paul M. Rice <158781635+paulmrice-72@users.noreply.github.com>
2026-04-03 22:34:36 -0500

    update production fixes

--- FF.WebBlazor/docker-entrypoint.sh
+++ FF.WebBlazor/docker-entrypoint.sh
@@ -1,7 +1,4 @@
-﻿#!/bin/sh
+#!/bin/sh
 if [ "$ASPNETCORE_ENVIRONMENT" = "Staging" ]; then
-  echo "Staging environment detected — swapping appsettings"
-  cp /usr/share/nginx/html/appsettings.Staging.json \
-     /usr/share/nginx/html/appsettings.json
+  cp /usr/share/nginx/html/appsettings.Staging.json /usr/share/nginx/html/appsettings.json
 fi
-exec nginx -g "daemon off;"
