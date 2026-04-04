2ac83ce013029d0eefb31aa9e60b604ff16dfdb2
Paul M. Rice <158781635+paulmrice-72@users.noreply.github.com>
2026-04-03 19:39:25 -0500

    migration to fix index

---
+++ FF.Infrastructure/Persistence/SQL/Migrations/20260404003658_Fix_Rosters_CompositeUniqueIndex.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FF.Infrastructure.Persistence.SQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_Rosters_CompositeUniqueIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
