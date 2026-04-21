4985eb24e9fff37c9cc68a895101d966b518a8e0
Paul M. Rice <158781635+paulmrice-72@users.noreply.github.com>
2026-04-20 20:15:00 -0500

    clean up war room brief; handled site reload (#586)

--- FF.WebBlazor/Services/AuthorizationMessageHandler.cs
+++ FF.WebBlazor/Services/AuthorizationMessageHandler.cs
@@ -1,15 +1,15 @@
-﻿using Microsoft.AspNetCore.Components.WebAssembly.Http;
+﻿// FF.WebBlazor/Services/AuthorizationMessageHandler.cs
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 namespace FF.WebBlazor.Services;
 
-public class AuthorizationMessageHandler(TokenStore tokenStore)
-    : DelegatingHandler
+public class AuthorizationMessageHandler(TokenStore tokenStore) : DelegatingHandler
 {
     protected override async Task<HttpResponseMessage> SendAsync(
         HttpRequestMessage request,
         CancellationToken cancellationToken)
     {
-        var token = tokenStore.AccessToken;
+        var token = await tokenStore.GetAccessTokenAsync();
         if (!string.IsNullOrEmpty(token))
             request.Headers.Authorization =
                 new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
